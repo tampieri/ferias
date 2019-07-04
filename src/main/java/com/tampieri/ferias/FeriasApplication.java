f39dd5d8766c958db266b461620777ed4a0a6eb0
tampieri <higortampieri@gmail.com>
2019-07-03 23:20:59 -0300

    Criação do projeto Ferias

---
+++ src/main/java/com/tampieri/ferias/FeriasApplication.java
@@ -0,0 +1,13 @@
+package com.tampieri.ferias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriasApplication.class, args);
+	}
+
+}
